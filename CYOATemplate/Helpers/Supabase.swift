9cc9ba17eea7602300c5f027244c2a83eed69a49
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2025-05-22 14:52:12 -0400

    Switched database to the correct one

--- CYOATemplate/Helpers/Supabase.swift
+++ CYOATemplate/Helpers/Supabase.swift
@@ -9,7 +9,7 @@ import Foundation
 import Supabase
 
 let supabase = SupabaseClient(
-  supabaseURL: URL(string: "https://mrdjbpjpvgtqxznsgtee.supabase.co")!,
-  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yZGpicGpwdmd0cXh6bnNndGVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTczMzIyMzgsImV4cCI6MjAzMjkwODIzOH0.kLM3U980_U3qjFvw-5kvUERqSsIiB_-HTHa2FlhM5wU"
+  supabaseURL: URL(string: "https://uwpbwxxjjutbpwrjcklo.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3cGJ3eHhqanV0YnB3cmpja2xvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MTk4NTUsImV4cCI6MjA2MzQ5NTg1NX0.AfwCkLPD4AkvwdV7R9SVbq_5wrTCQZGT2WuKe0x_XHg"
 )
 
