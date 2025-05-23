f46a82fa6a7eced84875478a980d8fa9cec891a3
Yukun Xie <yxie@lcs.on.ca>
2025-05-23 12:42:14 -0400

    Created new cover page for the application

--- CYOATemplate/Views/CoverView.swift
+++ CYOATemplate/Views/CoverView.swift
@@ -11,37 +11,53 @@ struct CoverView: View {
     
     // MARK: Stored properties
     
-    // Access the book state through the environment
+    // MARK: Stored properties
     @Environment(BookStore.self) var book
     
     // MARK: Computed properties
     var body: some View {
-        VStack {
-            
-            if book.isNotReadyToRead {
-                
-                ProgressView()
-                
-            } else {
-                
-                // Show the cover
-                Text("Journey Under the Sea")
-                    .font(.largeTitle)
-                
-                Button {
-                    // Animate page changes (fade)
-                    withAnimation {
-                        book.beginReading()
+        ZStack {
+            // Background gradient
+            LinearGradient(
+                gradient: Gradient(colors: [
+                                    Color("grad").opacity(1),
+                                    Color("grad").opacity(1),
+                                    Color("grad").opacity(0.6)
+                                ]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+
+            VStack {
+                // Cover image with aspect ratio preserved
+                Image("cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxHeight: .infinity)
+                    .padding()
+
+                if !book.isNotReadyToRead {
+                    VStack(spacing: 20) {
+
+                        Button {
+                            withAnimation {
+                                book.beginReading()
+                            }
+                        } label: {
+                            Text("Begin reading")
+                                .padding()
+                                .background(Color.white.opacity(0.8))
+                                .foregroundColor(.black)
+                                .cornerRadius(10)
+                        }
                     }
-                } label: {
-                    Text("Begin reading")
+                    .padding(.bottom, 50)
+                } else {
+                    ProgressView()
                 }
-                .buttonStyle(.borderedProminent)
             }
-            
         }
-        .padding()
-        
     }
 }
 
